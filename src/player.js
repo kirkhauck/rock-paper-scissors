6b8d2524f48857acb6c6545d995b144c935c6d43
Kirk Hauck <kirkihauck@gmail.com>
2023-01-11 16:03:27 -0700

    feat: Add Player.takeTurn() functionality

--- src/player.js
+++ src/player.js
@@ -6,7 +6,15 @@ class Player {
     this.score = 0;
   }
 
-  takeTurn() {
-    
-  }
+  takeTurn(event) {
+    if (this.name === 'Human' && event.target.classList.contains('fighter')) {
+      this.fighter = event.target.id;
+    } else if (board.difficulty === 'easy') {
+      var fighterIndex = Math.floor(Math.random() * easyFighters.length);
+      this.fighter = easyFighters[fighterIndex];
+    } else {
+      var fighterIndex = Math.floor(Math.random() * hardFighters.length);
+      this.fighter = hardFighters[fighterIndex];
+    }
+  } 
 }
